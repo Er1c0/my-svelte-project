a435cbe4da61ae76f81f71f0fa0036b9a9a4da3c
er1c0 <1627967013@qq.com>
2021-01-25 17:51:46 +0800

    add clickButton

---
+++ src/ClickButton.svelte
@@ -0,0 +1,14 @@
+<script>
+  let value=0;
+
+  function handleClick() {
+    value += 1;
+  }
+</script>
+<style>
+</style>
+<button on:click={handleClick}>
+
+  Click
+</button>
+<input type="text" style="width:100px;height: 180px;" value={value}>
